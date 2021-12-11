2495ead5616fecc9391bf53050d5305e5565065f
abdelslam ahmed <abuelwafaabdelsalam@gmail.com>
2021-12-11 22:20:07 +0200

    setting box

--- js/master.js
+++ js/master.js
@@ -1,3 +1,11 @@
+/* > setting box*/
+let toogle=()=>{
+    document.getElementById("setting") .classList.toggle("show")
+    document.querySelector(".fa-cog").classList.toggle("fa-spin")
+}     
+
+
+
 /* > background looping */
 let landingpage = document.querySelector(".landin-page")
 let arr = [1,2,3,4,5,6,7,8] // index of the img 
@@ -5,7 +13,4 @@ let looping=()=>{
     let random = Math.floor(Math.random()* arr.length); // random index selected
     landingpage.style.backgroundImage   =`url(../img/background/${arr[random]}.jpg)` // change the background-image to random img
 }
-
-setInterval(looping, 1000); // change img every 1s
-/* < background looping */
-
+setInterval(looping, 5000); // change img every 1s
