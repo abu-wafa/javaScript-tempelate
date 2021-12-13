2e055389ebb4c841bc58be62ac04c332051cd205
abdelslam ahmed <abuelwafaabdelsalam@gmail.com>
2021-12-13 21:12:28 +0200

    color box

--- js/master.js
+++ js/master.js
@@ -1,8 +1,37 @@
+//local storage
+let mainColor = localStorage.getItem("color-options")
+let colorLi = document.querySelectorAll(".setting-bar .color-box ul li")
+
+if(mainColor!==null){
+    document.documentElement.style.setProperty('--links-color',mainColor)
+    colorLi.forEach(e=>{
+        e.classList.remove("active")
+        if(e.dataset.color == mainColor){
+            e.classList.add("active")
+        }
+    })
+} 
 /* > setting box*/
 let toogle=()=>{
-    document.getElementById("setting") .classList.toggle("show")
+    document.getElementById("setting").classList.toggle("show")
     document.querySelector(".fa-cog").classList.toggle("fa-spin")
 }     
+
+
+/* > color box */
+
+colorLi.forEach(li=>{
+    li.addEventListener("click",function(e){
+    //move active class to the clicked li and remove from the other 
+        colorLi.forEach(li=>li.classList.remove("active"))
+        this.classList.add("active")
+    // switch the page color 
+        document.documentElement.style.setProperty('--links-color',e.target.dataset.color)
+    // add to the local storage    
+        localStorage.setItem("color-options",e.target.dataset.color)
+    
+    })
+})
 
 
 /* > background looping */
@@ -12,4 +41,4 @@ let looping=()=>{
     let random = Math.floor(Math.random()* arr.length); // random index selected
     landingpage.style.backgroundImage =`url(./img/background/${arr[random]}.jpg)` // change the background-image to random img
 }
-setInterval(looping, 5000); // change img every 1s
+setInterval(looping, 5000); // change img every 5s
