1b90db5676c6cf50fbf69cb269dfaa6aa267d55b
abdelslam ahmed <abuelwafaabdelsalam@gmail.com>
2021-12-15 00:19:47 +0200

    random background options js

--- js/master.js
+++ js/master.js
@@ -1,6 +1,12 @@
+//selctors 
+let landingpage = document.querySelector(".landin-page")
+let colorLi = document.querySelectorAll(".setting-bar .option-box ul li")
+let randombgSP = document.querySelectorAll(".setting-bar .random-color span")
+// glopal decleration
+let option = true;
+let backgroundInterval;
 //local storage
 let mainColor = localStorage.getItem("color-options")
-let colorLi = document.querySelectorAll(".setting-bar .color-box ul li")
 
 if(mainColor!==null){
     document.documentElement.style.setProperty('--links-color',mainColor)
@@ -16,10 +22,7 @@ let toogle=()=>{
     document.getElementById("setting").classList.toggle("show")
     document.querySelector(".fa-cog").classList.toggle("fa-spin")
 }     
-
-
-/* > color box */
-
+/* > colors box */
 colorLi.forEach(li=>{
     li.addEventListener("click",function(e){
     //move active class to the clicked li and remove from the other 
@@ -34,11 +37,36 @@ colorLi.forEach(li=>{
 })
 
 
+/* > dynamic images */
+randombgSP.forEach(Element=>{
+    Element.addEventListener("click",function(e){
+    //move active class to the clicked span and remove from the other 
+        randombgSP.forEach(elm=>elm.classList.remove("active"))
+        this.classList.add("active")
+
+        if(e.target.dataset.background==='Yes'){
+            option=true
+            randombackground()
+        }
+        else{
+            option=false
+            randombackground()
+        }
+    })
+})
 /* > background looping */
-let landingpage = document.querySelector(".landin-page")
 let arr = [1,2,3,4,5,6,7,8] // index of the img 
 let looping=()=>{ 
     let random = Math.floor(Math.random()* arr.length); // random index selected
     landingpage.style.backgroundImage =`url(./img/background/${arr[random]}.jpg)` // change the background-image to random img
 }
-setInterval(looping, 5000); // change img every 5s
+/* > random background options */
+let randombackground = function(){
+    if (option==true){
+        backgroundInterval = setInterval(looping, 1000); // change img every 5s
+    } 
+    else{
+        clearInterval(backgroundInterval)
+    }
+}
+randombackground()
